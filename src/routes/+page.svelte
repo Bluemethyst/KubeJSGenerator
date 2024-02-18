99a88110b8224072e67c14c00f535174b95283ac
blue <bluemethyst@gmail.com>
2024-02-19 08:47:07 +1300

    Started Port to SvelteKit :D

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<title>KubeJS Gen</title>
+<div class="main">
+	<h1>Use this simple website to generate basic KubeJS scripts</h1>
+	<h3>Fill out the following fields depending on what you want!</h3>
+	<h4>
+		Please refer to the full <a href="https://kubejs.com/wiki/">wiki</a> for more info as some attributes
+		are incomplete
+	</h4>
+	<h4>
+		Report all issues and submit feature requests to the <a
+			href="https://github.com/Bluemethyst/KubeJSGenerator">Github</a
+		>
+	</h4>
+</div>
--- tests.js
+++
@@ -1,7 +0,0 @@
-StartupEvents.registry('block', event => {
-
-	event.create('asd')
-	.displayName('asds')
-	.blockSound('coral_block')
-
-})
